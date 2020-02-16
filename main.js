e8181dc046cf6f4893cad13bf7f8d00710756e3d
Utsav Kuinkel <utshaav583@gmail.com>
2020-02-16 16:28:26 +0545

    Spelling fixed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Utsav", "a learner", "python developer", "niave web developer"];
+const textArray = ["Utsav", "a learner", "python developer", "naive web developer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
