109230dad58cbb9fad3d44eea5361fcf4c6f4191
utshaav025 <utshaav583@gmail.com>
2020-02-17 21:17:58 +0545

    Migrated script from index to js file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Utsav", "a learner", "python developer", "niave web developer"];
+const textArray = [
+  "Utsav",
+  "a learner",
+  "python developer",
+  "niave web developer"
+];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -10,53 +15,64 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
+    setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
     charIndex--;
     setTimeout(erase, erasingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
     textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
-new TypeIt("#type", {
-  strings: ["utshaav","utshaav_","uk_025","utshaav","utshaav025","utshaav_025"] ,
-  breakLines: false,
-  loop:true,
-  nextStringDelay: 5000,
-  loopDelay: 5000,
+document.addEventListener("DOMContentLoaded", function() {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+  new TypeIt("#type", {
+    strings: [
+      "utshaav",
+      "utshaav_",
+      "uk_025",
+      "utshaav",
+      "utshaav025",
+      "utshaav_025"
+    ],
+    breakLines: false,
+    loop: true,
+    nextStringDelay: 5000,
+    loopDelay: 5000
   }).go();
 
-(function() {
-    var rotator = document.getElementById('Images') //get the element
-    var delayInSeconds = 6                        //delay in seconds
-    var num = 1                                      //start number
-    var len = 6                                      //limit
-    setInterval(function(){                           //interval changer
-        num = (num === len) ? 0 : num;                //reset if limit reached
-        rotator.src = 'images/' + num + '.png';                     //change picture
-        console.log(rotator.src)
-        num++;                                        //increment counter
+  (function() {
+    var rotator = document.getElementById("Images"); //get the element
+    var delayInSeconds = 6; //delay in seconds
+    var num = 1; //start number
+    var len = 6; //limit
+    setInterval(function() {
+      //interval changer
+      num = num === len ? 0 : num; //reset if limit reached
+      rotator.src = "images/" + num + ".png"; //change picture
+      console.log(rotator.src);
+      num++; //increment counter
     }, delayInSeconds * 1000);
-    }());
+  })();
+});
